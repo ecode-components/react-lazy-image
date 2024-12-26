37e764908f3e1c797b416af8bac815bf7e5c6f71
kaiyu.kong <kaiyu.kong@ximalaya.com>
2024-12-26 16:18:28 +0800

    add

--- .changeset/commit-template.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  // getAddMessage: (arg) => {
-  //   return `feat: ${arg.summary}`
-  // },
-  getVersionMessage: (arg) => {
-    return `build: ${arg.changesets[0].summary}`
-  }
-}
